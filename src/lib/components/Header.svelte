b035ef917b3bc7c92e2670ff3ae8c771d1ed7d39
Simone Santoro <simone.santoro@eui.eu>
2024-01-26 16:54:02 +0100

    Added notifications button/dropdown

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,6 +4,7 @@
 
 	export let data;
 	let isProfileDropdownOpen = false;
+	let isNotificationDropdownOpen = false;
 	let publicProfileUrl;
 
 	const user_data = data.session.algoliaUser;
@@ -20,6 +21,10 @@
 	function toggleProfileDropdown() {
 		isProfileDropdownOpen = !isProfileDropdownOpen;
 	}
+
+	function toggleNotificationDropdown() {
+		isNotificationDropdownOpen = !isNotificationDropdownOpen;
+	}	
 </script>
 
 <header class="header bg-white shadow py-4 px-4">
@@ -33,12 +38,29 @@
 			</div>
 			<div class="flex md:hidden">
 				<a href="/" class="flex items-center justify-center h-10 w-10 border-transparent">
-				<a href="/" class="flex items-center justify-center h-10 w-10 border-transparent">
-				<span class="fa-solid fa-magnifying-glass"></span>
+					<span class="fa-solid fa-magnifying-glass"></span>
 				</a>
 			</div>
 		</form>
 		<div class="flex ml-auto items-center">
+			<div class="relative inline-block text-left">
+				<div class="flex flex-row items-center justify-end h-8 me-7 pe-7 border-e border-gray-400 text-gray-600">
+					<button type="button" on:click={toggleNotificationDropdown} class="relative duration-300 w-8 h-8 bg-gray-100 rounded-full" id="notification-button" aria-expanded="false" aria-haspopup="true">
+						<span class="sr-only">Notifications</span>
+						<span class="fa-regular fa-bell fa-lg" aria-hidden="true"></span>
+						<span class="absolute flex items-center justify-center bg-red-600 text-red-100 h-5 min-w-5 text-xs font-bold rounded-full -top-1 -right-3">99</span>
+					</button>
+				</div>
+				<div id="notification-dropdown" class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none {isNotificationDropdownOpen ? 'block' : 'hidden'}" role="menu" aria-orientation="vertical" aria-labelledby="notification-button" tabindex="-1">
+					<div class="py-1" role="none">
+						<!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
+						<a href="/" class="text-gray-700 block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-0">Announcement #1</a>
+						<a href="/" class="text-gray-700 block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-0">Announcement #2</a>
+						<a href="/" class="text-gray-700 block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-0">Announcement #3</a>
+					</div>
+
+				</div>				
+			</div>
 			<div class="flex flex-row items-center justify-end me-2">
 				<img src="{portrait}" alt class="h-10 w-10 bg-gray-200 rounded-full" />
 				<span class="flex flex-col ml-2">
@@ -50,7 +72,7 @@
 				  <span class="sr-only">EUI profile: {user_data.ict.Firstnames} {user_data.ict.Lastnames}</span>
 				  <span class="fa-regular fa-angle-down {isProfileDropdownOpen ? 'fa-rotate-180' : ''}" aria-hidden="true"></span>
 				</button>
-				<div id="profile-dropdown" class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none {isProfileDropdownOpen ? 'block' : 'hidden'}" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+				<div id="profile-dropdown" class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none {isProfileDropdownOpen ? 'block' : 'hidden'}" role="menu" aria-orientation="vertical" aria-labelledby="profile-button" tabindex="-1">
 					<div class="py-1" role="none">
 						<!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
 						<a href="{publicProfileUrl}" class="text-gray-700 block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-0">View your public profile</a>

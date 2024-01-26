1697a512d94e38d1fe0ec933f041210c934e2a4c
Simone Santoro <simone.santoro@eui.eu>
2024-01-26 15:47:02 +0100

    Added public profile url to profile dropdown

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,19 +1,25 @@
 <script>
-  import { PUBLIC_EUI_WEB } from '$env/static/public';
-  import { signOut } from "@auth/sveltekit/client";
+	import { PUBLIC_EUI_WEB } from '$env/static/public';
+	import { signOut } from "@auth/sveltekit/client";
 
-  export let data;
-  let isProfileDropdownOpen = false;
+	export let data;
+	let isProfileDropdownOpen = false;
+	let publicProfileUrl;
 
-  const user_data = data.session.algoliaUser;
-  let portrait = '/default-user-dark.png';
-  if(user_data.cms.photo) {
-    portrait = PUBLIC_EUI_WEB + user_data.cms.photo.asset.sys.uri;
-  }
+	const user_data = data.session.algoliaUser;
+	let portrait = '/default-user-dark.png';
+	
+	if(user_data.cms.photo) {
+		portrait = PUBLIC_EUI_WEB + user_data.cms.photo.asset.sys.uri;
+	}
 
-  function toggleProfileDropdown() {
-    isProfileDropdownOpen = !isProfileDropdownOpen;
-  }
+	if(user_data.cms.sys.slug) {
+		publicProfileUrl = PUBLIC_EUI_WEB + '/people?id=' + user_data.cms.sys.slug;
+	}
+
+	function toggleProfileDropdown() {
+		isProfileDropdownOpen = !isProfileDropdownOpen;
+	}
 </script>
 
 <header class="header bg-white shadow py-4 px-4">
@@ -31,25 +37,22 @@
 				</a>
 			</div>
 		</form>
-		<div class="flex ml-auto">
-			<div class="flex flex-row items-center justify-end me-2 pe-4 border-e border-gray-500">
-				<img src="{portrait}" alt class="h-10 w-10 bg-gray-200 rounded-full" />
+		<div class="flex ml-auto items-center">
+			<div class="flex flex-row items-center justify-end me-2 pe-4">
+				<img src="{portrait}" alt class="h-8 w-8 bg-gray-200 rounded-full" />
 				<span class="flex flex-col ml-2">
-          <span class="truncate w-auto max-w-48 font-semibold tracking-wide leading-none">{user_data.ict.Firstnames} {user_data.ict.Lastnames}</span>
-          {#if user_data.ict.Staff}
-          <span class="truncate w-auto max-w-48 text-gray-500 text-sm leading-none mt-2">{user_data.ict.Staff.PostJobTitles[0].JobTitle}</span>
-          {/if}
+					<span class="truncate w-auto max-w-48 font-bold tracking-wide leading-none">{user_data.ict.Firstnames} {user_data.ict.Lastnames}</span>
 				</span>
 			</div>
 			<div class="relative inline-block text-left">
-				<button type="button" on:click={toggleProfileDropdown} class="inline-flex w-8 justify-center hover:bg-eui-blue hover:text-white" id="profile-button" aria-expanded="false" aria-haspopup="true">
+				<button type="button" on:click={toggleProfileDropdown} class="inline-flex w-8 h-8 items-center justify-center" id="profile-button" aria-expanded="false" aria-haspopup="true">
 				  <span class="sr-only">EUI profile: {user_data.ict.Firstnames} {user_data.ict.Lastnames}</span>
-				  <span class="fa-sharp fa-solid fa-grid fa-xl {isProfileDropdownOpen ? 'fa-rotate-180' : ''}" aria-hidden="true"></span>
+				  <span class="fa-regular fa-angle-down fa-lg {isProfileDropdownOpen ? 'fa-rotate-180' : ''}" aria-hidden="true"></span>
 				</button>
 				<div id="profile-dropdown" class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none {isProfileDropdownOpen ? 'block' : 'hidden'}" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
 					<div class="py-1" role="none">
 						<!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-						<a href="#" class="text-gray-700 block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-0">View your public profile</a>
+						<a href="{publicProfileUrl}" class="text-gray-700 block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-0">View your public profile</a>
 						<a href="#" class="text-gray-700 block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-1">Duplicate</a>
 					</div>
 					<div class="py-1" role="none">

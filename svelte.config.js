ff0fae51f9f0594713598bf45f59b603077f2377
Emanuele Strano <ilanurs@gmail.com>
2024-01-30 10:14:43 +0100

    Aggiunto adattatore Node.js per Svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,3 +8,4 @@ const config = {
   preprocess: vitePreprocess()
 };
 export default config;
+

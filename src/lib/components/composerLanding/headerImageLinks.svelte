67fae0439f9969bd39cc38801e5f11bccb005095
Simone Santoro <simone.santoro@eui.eu>
2024-02-01 10:37:55 +0100

    Added support for aspect ratio

--- src/lib/components/composerLanding/headerImageLinks.svelte
+++ src/lib/components/composerLanding/headerImageLinks.svelte
@@ -1,5 +1,5 @@
 <script>
-      import { PUBLIC_EUI_WEB } from '$env/static/public';
+    import { PUBLIC_EUI_WEB } from '$env/static/public';
     export let item;
     export let title;
 
@@ -8,7 +8,7 @@
 </script>
 
 
-<div class="grid grid-cols-3 gap-0">
+<div class="grid grid-cols-1 2xl:grid-cols-3 gap-0">
     <div class="relative col-span-2">
         <h1 class="absolute text-white drop-shadow-lg bottom-0 left-0">{title}</h1>
         <img src="{PUBLIC_EUI_WEB}/{item.image.asset.sys.uri}" alt="{item.image.altText}" />

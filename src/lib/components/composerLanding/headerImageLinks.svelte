105b4b6d670efd129b1f68136bdc42fb06977d37
Emanuele Strano <ilanurs@gmail.com>
2024-02-01 09:57:01 +0100

    Aggiornato il percorso dell'immagine nell'intestazione del collegamento all'immagine

--- src/lib/components/composerLanding/headerImageLinks.svelte
+++ src/lib/components/composerLanding/headerImageLinks.svelte
@@ -1,4 +1,5 @@
 <script>
+      import { PUBLIC_EUI_WEB } from '$env/static/public';
     export let item;
     export let title;
 
@@ -10,7 +11,7 @@
 <div class="grid grid-cols-3 gap-0">
     <div class="relative col-span-2">
         <h1 class="absolute text-white drop-shadow-lg bottom-0 left-0">{title}</h1>
-        <img src="https://www.eui.eu/{item.image.asset.sys.uri}" alt="{item.image.altText}" />
+        <img src="{PUBLIC_EUI_WEB}/{item.image.asset.sys.uri}" alt="{item.image.altText}" />
     </div>
     <div class="col-span-1">
         <div>{@html item.markup}</div>

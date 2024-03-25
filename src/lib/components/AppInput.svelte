fd5572f9af9ce98050d81b4c881be9fbdd2834d2
Pieter-Jan-iO <pieter-jan.deridder@iodigital.com>
2024-03-25 14:24:03 +0100

    Added AppInput component

---
+++ src/lib/components/AppInput.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let name;
+	export let label = '';
+	export let value = '';
+</script>
+
+<div class="flex flex-col">
+	<label for={name}>{label}</label>
+	<input
+		bind:value
+		{name}
+		{...$$restProps}
+		class="rounded disabled:cursor-not-allowed disabled:bg-eui-gray"
+	/>
+</div>
---
+++ src/routes/[userSlug]/create/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import AppInput from '$lib/components/AppInput.svelte';
+
+	export let data;
+
+	console.log('SESSION', data.user);
+</script>
+
+<div class="container py-16">
+	<form action="" class="w-fit min-w-60 space-y-5">
+		<AppInput name="name" label="Name" value={data.user.cms.nameAndSurnameForTheWeb} disabled />
+		<AppInput name="email" type="email" label="E-mail" value={data.user.cms.email} disabled />
+	</form>
+</div>

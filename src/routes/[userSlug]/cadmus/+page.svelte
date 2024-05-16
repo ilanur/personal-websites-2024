f09a3f242038ac068250a08a1e3097252be180f3
Emanuele Strano <ilanurs@gmail.com>
2024-05-16 18:59:08 +0200

    Aggiunti pacchetti xml2js e sax

---
+++ src/routes/[userSlug]/cadmus/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let publications;
+
+</script>
+<div class="publications-container">
+	{#each publications as pub}
+	  <div class="publication">
+		<h3>{pub.title}</h3>
+		<p>{pub.author}</p>
+		<a href="{pub.url}" target="_blank">Read More</a>
+		<p>Published: {pub.date}</p>
+	  </div>
+	{/each}
+  </div>
+  

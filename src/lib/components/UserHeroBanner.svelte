bb455af71664f4ea67de8aae891f815345b0f013
Simone <simone.santoro@eui.eu>
2024-12-03 15:58:24 +0100

    Styled header and removed duplicated social links for mobile

--- src/lib/components/UserHeroBanner.svelte
+++ src/lib/components/UserHeroBanner.svelte
@@ -9,47 +9,55 @@
 	let { personalWebsite, isSmall } = $props()
 </script>
 
+<div class="relative bg-eui-dark-blue-500 pt-0 md:bg-[transparent] md:py-16">
+	<HeroBannerGraphic class="absolute bottom-0 left-0 top-0 z-10 m-auto ml-5 hidden w-fit text-white opacity-10 xl:block" />
+	<div class="container relative z-10 grid grid-cols-12 pb-[calc(125px_+_2rem)] pt-8 md:py-16">
+		<div class="col-span-12 text-center text-white md:col-span-8 md:pr-[150px] md:text-start lg:pr-[200px] xl:pr-[300px]">
+			<h1 class="mt-0 text-3xl">{personalWebsite.title}</h1>
+			<div class="mt-4">
+				{@html personalWebsite.description}
+			</div>
+		</div>
+	</div>
+	<div class="bottom-0 left-0 right-0 top-0 grid grid-cols-12 md:absolute">
+		<div class="col-span-12 hidden bg-eui-dark-blue-500 md:col-span-8 md:block"></div>
+		<div class="col-span-12 flex flex-col items-center justify-center bg-intranet-gray-100 pb-12 md:col-span-4 md:flex-row md:justify-start md:pb-0">
+			<div class="-mt-[125px] size-[250px] min-w-[250px] overflow-hidden rounded-full border-8 border-white bg-intranet-gray-100 text-eui-dark-blue-600 md:-ml-[150px] md:mr-6 md:mt-0 md:size-[300px] md:min-w-[300px]">
+				<img alt="user" class="size-full object-cover" src={getThumbnail(personalWebsite.image)} />
+			</div>
+			{#if personalWebsite.socialMedia.length}
+				<UserHeroBannerSocials socials={personalWebsite.socialMedia} class="z-20 mt-4 flex max-h-[300px] gap-4 border-t border-slate-400 pt-4 md:mt-0 md:flex md:flex-col md:flex-wrap md:border-s md:border-t-0 md:ps-6 md:pt-0" />
+			{/if}
+		</div>
+	</div>
+</div>
+
+<!--
 <div>
 	<div
-		class={clsx('bg-eui-dark-blue-500 relative pt-0 md:bg-[transparent]', {
+		class={clsx('relative bg-eui-dark-blue-500 pt-0 md:bg-[transparent]', {
 			'md:py-8': isSmall,
 			'md:py-20': !isSmall
 		})}
 	>
 		{#if !isSmall}
-			<HeroBannerGraphic
-				class="absolute bottom-0 left-0 top-0 z-10 m-auto ml-5 hidden w-fit text-white opacity-10 xl:block"
-			/>
+			<HeroBannerGraphic class="absolute bottom-0 left-0 top-0 z-10 m-auto ml-5 hidden w-fit text-white opacity-10 xl:block" />
 		{:else}
-			<HeroBannerGraphicSmall
-				class="absolute bottom-0 left-0 top-0 z-10 m-auto ml-5 hidden text-white opacity-10 xl:block"
-			/>
-			<HeroBannerGraphicSmall
-				class="text-eui-dark-blue-600 absolute bottom-0 right-0 top-0 z-10 m-auto mr-5 hidden xl:block"
-			/>
+			<HeroBannerGraphicSmall class="absolute bottom-0 left-0 top-0 z-10 m-auto ml-5 hidden text-white opacity-10 xl:block" />
+			<HeroBannerGraphicSmall class="absolute bottom-0 right-0 top-0 z-10 m-auto mr-5 hidden text-eui-dark-blue-600 xl:block" />
 		{/if}
 
 		<div class="bottom-0 left-0 right-0 top-0 grid grid-cols-12 md:absolute">
-			<div class="bg-eui-dark-blue-500 col-span-12 hidden md:col-span-8 md:block"></div>
-			<div
-				class="bg-intranet-gray-100 col-span-12 flex items-center justify-center pt-12 md:col-span-4 md:justify-start md:pt-0"
-			>
+			<div class="col-span-12 hidden bg-eui-dark-blue-500 md:col-span-8 md:block"></div>
+			<div class="col-span-12 flex items-center justify-center bg-intranet-gray-100 pt-12 md:col-span-4 md:justify-start md:pt-0">
 				<div
-					class={clsx(
-						'text-eui-dark-blue-600 bg-intranet-gray-100 overflow-hidden rounded-full border-8 border-white md:mb-0 md:mr-9',
-						{
-							'-mb-[84px] size-[168px] min-w-[168px] md:-ml-[84px]': isSmall,
-							'-mb-[125px] size-[250px] min-w-[250px] md:-ml-[150px] md:size-[300px] md:min-w-[300px]':
-								!isSmall
-						}
-					)}
+					class={clsx('overflow-hidden rounded-full border-8 border-white bg-intranet-gray-100 text-eui-dark-blue-600 md:mb-0 md:mr-9', {
+						'-mb-[84px] size-[168px] min-w-[168px] md:-ml-[84px]': isSmall,
+						'-mb-[125px] size-[250px] min-w-[250px] md:-ml-[150px] md:size-[300px] md:min-w-[300px]': !isSmall
+					})}
 				>
 					{#if personalWebsite.image}
-						<img
-							src={getThumbnail(personalWebsite.image)}
-							alt="user"
-							class="size-full object-cover"
-						/>
+						<img src={getThumbnail(personalWebsite.image)} alt="user" class="size-full object-cover" />
 					{:else}
 						<div
 							class={clsx({
@@ -63,10 +71,7 @@
 				</div>
 
 				{#if !isSmall}
-					<UserHeroBannerSocials
-						socials={personalWebsite.socialMedia}
-						class="z-20 hidden max-h-[300px] flex-col flex-wrap gap-4 md:flex"
-					/>
+					<UserHeroBannerSocials socials={personalWebsite.socialMedia} class="z-20 hidden max-h-[300px] flex-col flex-wrap gap-4 md:flex" />
 				{/if}
 			</div>
 		</div>
@@ -93,16 +98,14 @@
 				{#if personalWebsite.socialMedia.length}
 					<UserHeroBannerSocials
 						socials={personalWebsite.socialMedia}
-						class={clsx(
-							'mt-10 grid grid-cols-4 justify-between gap-12 border-t pt-10 sm:grid-cols-6 md:w-fit md:gap-x-6 md:gap-y-0 md:pt-6',
-							{
-								'md:flex': isSmall,
-								'md:hidden': !isSmall
-							}
-						)}
+						class={clsx('mt-10 grid grid-cols-4 justify-between gap-12 border-t pt-10 sm:grid-cols-6 md:w-fit md:gap-x-6 md:gap-y-0 md:pt-6', {
+							'md:flex': isSmall,
+							'md:hidden': !isSmall
+						})}
 					/>
 				{/if}
 			</div>
 		</div>
 	</div>
 </div>
+-->

2dad1585957fdd06425f4462e93ce6ccb7b30180
Pieter-Jan-iO <pieter-jan.deridder@iodigital.com>
2024-12-09 13:30:48 +0100

    Code cleanup

--- src/lib/utils/contensis/client.js
+++ src/lib/utils/contensis/client.js
@@ -3,18 +3,17 @@ import { PUBLIC_EUI_WEB } from '$env/static/public'
 
 export function getCanvasHTML(canvas) {
 	const mapped = canvas.map((el) => {
-		const obj = el
-
 		if (el.type === '_image' && el.value.asset.sys.uri.startsWith('/Content-Types-Assets/')) {
-			obj.value.asset.sys.uri = `${PUBLIC_EUI_WEB}${el.value.asset.sys.uri}`
+			el.value.asset.sys.uri = `${PUBLIC_EUI_WEB}${el.value.asset.sys.uri}`
 		}
 
-		return obj
+		return el
 	})
 
 	const renderer = createRenderer()
 	const getCanvasHtml = (data) => renderer({ data })
 	const html = getCanvasHtml(mapped)
+
 	return html
 }
 

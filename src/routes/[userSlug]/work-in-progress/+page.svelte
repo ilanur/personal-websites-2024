49a6b3c9b82616f22d9ffab6bb1d7c994e1d0bc9
Pieter-Jan-iO <pieter-jan.deridder@iodigital.com>
2024-12-17 14:40:29 +0100

    Add EditableContent to WIP page

--- src/routes/[userSlug]/work-in-progress/+page.svelte
+++ src/routes/[userSlug]/work-in-progress/+page.svelte
@@ -1,13 +1,23 @@
 <script>
 	import { getContext } from 'svelte'
-	import { getCanvasHTML } from '$lib/utils/contensis/client.js'
+	import { getCanvasHTML, savePageContent } from '$lib/utils/contensis/client.js'
+	import { PUBLIC_CONTENSIS_PAGES_ASSETS_FOLDER } from '$env/static/public'
+	import EditableContent from '$lib/components/EditableContent.svelte'
 
 	let { data } = $props()
 
 	const smallHeroBanner = getContext('smallHeroBanner')
 
+	let isAuthUserWebsite = $derived(data.personalWebsite.people.email === data.authUser?.email)
+	let isAdmin = $derived(data.authUser?.role?.includes('admin'))
+
 	$smallHeroBanner = true
 </script>
 
 <h1>{data.page.title}</h1>
-<div class="canvas-content">{@html data.page.canvas ? getCanvasHTML(data.page.canvas) : 'This page is empty'}</div>
+
+<EditableContent
+	htmlContent={data.page ? getCanvasHTML(data.page.canvas) : ''}
+	enabled={isAuthUserWebsite || isAdmin}
+	onSave={async (canvas) => await savePageContent(data.page, canvas, PUBLIC_CONTENSIS_PAGES_ASSETS_FOLDER)}
+/>
